76a9995295ca28c52b8433355df5f020a912a52b
vmpr7 <yuyu7vmpr@gmail.com>
2020-10-27 19:20:40 +0300

    new changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,44 @@
-// VARIABLES & PATHS
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		browserSync  = require('browser-sync').create(),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify-es').default,
+		cleancss     = require('gulp-clean-css'),
+		rename       = require('gulp-rename'),
+		autoprefixer = require('gulp-autoprefixer'),
+		rsync        = require('gulp-rsync'),
+		newer        = require('gulp-newer'),
+		responsive   = require('gulp-responsive'),
+		del          = require('del');
 
-let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
-    fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
-    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
-    baseDir      = 'app', // Base directory path without «/» at the end
-    online       = true; // If «false» - Browsersync will work offline without internet connection
-
-let paths = {
-
-	scripts: {
-		src: [
-			// 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-			baseDir + '/js/app.js' // app.js. Always at the end
-		],
-		dest: baseDir + '/js',
-	},
-
-	styles: {
-		src:  baseDir + '/' + preprocessor + '/main.*',
-		dest: baseDir + '/css',
-	},
-
-	images: {
-		src:  baseDir + '/images/src/**/*',
-		dest: baseDir + '/images/dest',
-	},
-
-	deploy: {
-		hostname:    'username@yousite.com', // Deploy hostname
-		destination: 'yousite/public_html/', // Deploy destination
-		include:     [/* '*.htaccess' */], // Included files to deploy
-		exclude:     [ '**/Thumbs.db', '**/*.DS_Store' ], // Excluded files from deploy
-	},
-
-	cssOutputName: 'app.min.css',
-	jsOutputName:  'app.min.js',
-
-}
-
-// LOGIC
-
-const { src, dest, parallel, series, watch } = require('gulp');
-const sass         = require('gulp-sass');
-const scss         = require('gulp-sass');
-const less         = require('gulp-less');
-const styl         = require('gulp-stylus');
-const cleancss     = require('gulp-clean-css');
-const concat       = require('gulp-concat');
-const browserSync  = require('browser-sync').create();
-const uglify       = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin     = require('gulp-imagemin');
-const newer        = require('gulp-newer');
-const rsync        = require('gulp-rsync');
-const del          = require('del');
-
-function browsersync() {
+// Local Server
+gulp.task('browser-sync', function() {
 	browserSync.init({
-		server: { baseDir: baseDir + '/' },
+		server: {
+			baseDir: 'app'
+		},
 		notify: false,
-		online: online
+		// online: false, // Work offline without internet connection
+		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
 	})
-}
+});
+function bsReload(done) { browserSync.reload(); done() };
 
-function scripts() {
-	return src(paths.scripts.src)
-	.pipe(concat(paths.jsOutputName))
-	.pipe(uglify())
-	.pipe(dest(paths.scripts.dest))
+// Custom Styles
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({
+		outputStyle: 'expanded',
+		includePaths: [__dirname + '/node_modules']
+	}))
+	.pipe(concat('styles.min.css'))
+	.pipe(autoprefixer({
+		// grid: true, // Optional. Enable CSS Grid
+		overrideBrowserslist: ['last 10 versions']
+	}))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
+	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream())
-<<<<<<< HEAD
 });
 
 // Scripts & JS Libraries
@@ -125,47 +94,19 @@ gulp.task('code', function() {
 // Deploy
 gulp.task('rsync', function() {
 	return gulp.src('app/')
-=======
-}
-
-function styles() {
-	return src(paths.styles.src)
-	.pipe(eval(preprocessor)())
-	.pipe(concat(paths.cssOutputName))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
-	.pipe(dest(paths.styles.dest))
-	.pipe(browserSync.stream())
-}
-
-function images() {
-	return src(paths.images.src)
-	.pipe(newer(paths.images.dest))
-	.pipe(imagemin())
-	.pipe(dest(paths.images.dest))
-}
-
-function cleanimg() {
-	return del('' + paths.images.dest + '/**/*', { force: true })
-}
-
-function deploy() {
-	return src(baseDir + '/')
->>>>>>> 1bc3afadd9df9fb7fcd4ee1e04609313c9f712c4
 	.pipe(rsync({
-		root: baseDir + '/',
-		hostname: paths.deploy.hostname,
-		destination: paths.deploy.destination,
-		include: paths.deploy.include,
-		exclude: paths.deploy.exclude,
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Included files
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
 		recursive: true,
 		archive: true,
 		silent: false,
 		compress: true
 	}))
-}
+});
 
-<<<<<<< HEAD
 gulp.task('icons', function() {
 	return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
 			.pipe(gulp.dest('./app/fonts/fontawesome'));
@@ -179,20 +120,3 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', gulp.parallel('img', 'styles', 'scripts', 'icons', 'browser-sync', 'watch'));
-=======
-function startwatch() {
-	watch(baseDir  + '/' + preprocessor + '/**/*', {usePolling: true}, styles);
-	watch(baseDir  + '/images/src/**/*.{' + imageswatch + '}', {usePolling: true}, images);
-	watch(baseDir  + '/**/*.{' + fileswatch + '}', {usePolling: true}).on('change', browserSync.reload);
-	watch([baseDir + '/js/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], {usePolling: true}, scripts);
-}
-
-exports.browsersync = browsersync;
-exports.assets      = series(cleanimg, styles, scripts, images);
-exports.styles      = styles;
-exports.scripts     = scripts;
-exports.images      = images;
-exports.cleanimg    = cleanimg;
-exports.deploy      = deploy;
-exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
->>>>>>> 1bc3afadd9df9fb7fcd4ee1e04609313c9f712c4
